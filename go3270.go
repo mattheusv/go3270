a43d7a3ca40057e23f9514afb8bde257500be83f
Matheus Alcantara <matheusssilv972gmail.com>
2018-11-13 22:54:25 -0200

    improvement doc

--- go3270.go
+++ go3270.go
@@ -1,9 +1,9 @@
 /*
-Package go3270 is a 3270 terminal emulator and screen-scraping library written entirely Go.
+Package go3270 is a x3270 terminal emulator and screen-scraping library written entirely in Go.
 
 Go3270 use x3270 scripts to emulators terminal, so, you need to set x3270 in your $PATH to go3270 works.
 */
 package go3270
 
 //version of package - based on Semantic Versioning 2.0.0 http://semver.org/
-const version = "0.0.0"
+const version = "0.1.0"
