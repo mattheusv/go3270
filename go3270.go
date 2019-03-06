417941edb6b7d60c7c8f29c642ccbd64e31a7948
Matheus Alcantara <matheusssilv97@gmail.com>
2019-03-06 11:41:58 -0300

    add example to godoc

--- go3270.go
+++ go3270.go
@@ -2,6 +2,54 @@
 Package go3270 is a x3270 terminal emulator and screen-scraping library written entirely in Go.
 
 Go3270 use x3270 scripts to emulators terminal, so, you need to set x3270 in your $PATH to go3270 works.
+
+Example:
+	package main
+
+	import (
+		"log"
+
+		"github.com/msalcantara/go3270"
+	)
+
+	func main() {
+
+		e := go3270.Emulator{
+			Host: "0.0.0.0",
+			Port: 992,
+		}
+
+		if err := e.Connect(); err != nil {
+			log.Fatalf("error to create connection: %v\n", err)
+		}
+
+		if err := e.SetString("my_user"); err != nil {
+			log.Fatalf("error to set user: %v\n", err)
+		}
+
+		if err := e.Press(go3270.Tab); err != nil {
+			log.Fatalf("error to press tab: %v\n", err)
+		}
+
+		if err := e.SetString("my_password"); err != nil {
+			log.Fatalf("error to set password: %v\n", err)
+		}
+
+		if err := e.Press(go3270.Enter); err != nil {
+			log.Fatalf("error to press enter: %v\n", err)
+		}
+
+		v, err := e.GetValue(0, 1, 4)
+		if err != nil {
+			log.Fatalf("error to get value: %v", err)
+		}
+		log.Println(v)
+
+		if err := e.Disconnect(); err != nil {
+			log.Fatalf("error to disconnect: %v\n", err)
+		}
+
+	}
 */
 package go3270
 
